75f430bd81953e7662e3a87a3a62086c955c51a6
Chris Getsfred <cgetsfred@ediscovery.co>
2021-09-29 11:27:52 -0500

    Remove classnames

--- packages/site-one/src/App.tsx
+++ packages/site-one/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <h1>Site one</h1>
       </header>
     </div>
